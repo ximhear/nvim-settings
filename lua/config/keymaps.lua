5860c3a9c359c3d4a8615e3e172d018d435db367
gzonelee <ximhear@gmail.com>
2026-03-12 22:55:39 +0900

    leader tv로 right terminal 기능 추가

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- Terminal: open on the right side
+vim.keymap.set("n", "<leader>tv", function()
+  Snacks.terminal(nil, { win = { position = "right", width = 0.4 } })
+end, { desc = "Terminal (right)" })
